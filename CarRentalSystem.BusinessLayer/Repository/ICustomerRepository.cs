778d570bf8751a453ab3b929429285a785298c86
AdhyatmCodes27 <98180845+AdhyatmCodes27@users.noreply.github.com>
2024-10-14 14:09:18 +0530

    Add files via upload
    C# case study - CarRentalSystem

---
+++ CarRentalSystem.BusinessLayer/Repository/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalSystem.Entity;
+
+namespace CarRentalSystem.BusinessLayer.Repository
+{
+    public interface ICustomerRepository
+    {
+        void AddCustomer(Customer customer);
+        void RemoveCustomer(int customerID);
+        List<Customer> ListCustomers();
+        Customer FindCustomerById(int customerID);
+    }
+}
---
+++ CarRentalSystem.BusinessLayer/Repository/VehicleRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalSystem.Entity;
+
+namespace CarRentalSystem.BusinessLayer.Repository
+{
+    public class VehicleRepository: IVehicleRepository
+    {
+        
+        private List<Vehicle> cars = new List<Vehicle>();
+
+        public VehicleRepository(List<Vehicle> vehicles)
+        {
+        }
+
+        public void AddCar(Vehicle car)
+        {
+            cars.Add(car);
+        }
+
+        public void RemoveCar(int carID)
+        {
+            var car = FindCarById(carID);
+            if (car != null)
+            {
+                cars.Remove(car);
+            }
+        }
+
+        public List<Vehicle> ListAvailableCars()
+        {
+            return cars.Where(c => c.Status == "available").ToList();
+        }
+
+        public List<Vehicle> ListRentedCars()
+        {
+            return cars.Where(c => c.Status == "notAvailable").ToList();
+        }
+
+        public Vehicle FindCarById(int carID)
+        {
+            var car = cars.FirstOrDefault(c => c.VehicleID == carID);
+            if (car == null)
+            {
+                throw new Exception($"Car with ID {carID} not found.");
+            }
+            return car;
+        }
+    }
+}
